fixed  scaling issue

src/predictor.py
@@ -3,6 +3,8 @@ import argparse
 import glob
 import joblib
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
 
 
 def get_af2_emb_upd(colabfold_output_dir: str, model_id: int, use_pairwise: bool):
@@ -27,19 +29,20 @@ def predict_oligo_state(colabfold_output_dir:  str, use_pairwise: bool):
     results = []
     for i in range(0,5):
         X = np.asarray([get_af2_emb_upd(colabfold_output_dir, model_id = i, use_pairwise=use_pairwise)])
+        sc = model[f'scaler_0_{i}']
+        X= sc.transform(X)
         result = model[f'clf_0_{i}'].predict_proba(X)
         results.append(result)
 
     results = np.array(results)
-    # print(results)
 
-    avg_proba = np.mean(results, axis=0)
-    std_proba = np.std(results, axis=0)
+    avg_proba = np.mean(results, axis=1)
+    std_proba = np.std(results, axis=1)
 
     y_pred_bin = avg_proba.argmax(axis=1)
     # print(avg_proba, std_proba, y_pred_bin)
 
-    # data = {avg_proba[0][0]: std_proba[0][0], avg_proba[0][1]: std_proba[0][1], avg_proba[0][2]: std_proba[0][2]}
+    data = {avg_proba[0][0]: std_proba[0][0], avg_proba[0][1]: std_proba[0][1], avg_proba[0][2]: std_proba[0][2]}
     data = {'prob_dimer':avg_proba[:,1],
             'prob_dimer_std':std_proba[:,0],
              'prob_trimer':avg_proba[:,1],
@@ -52,7 +55,7 @@ def predict_oligo_state(colabfold_output_dir:  str, use_pairwise: bool):
     df.to_csv('oligo_pred.csv')
     oligo_dict = {0: "Dimer", 1: "Trimer", 2: "Tetramer"}
 
-    print(f"Predicted oligomer state: {oligo_dict[y_pred_bin[0]]} {y_pred_bin[0]} with probability \
-          {round(avg_proba[0][y_pred_bin[0]],3)} +/- {round(std_proba[0][y_pred_bin[0]],3)}")
+    print(f"Predicted oligomer state: {oligo_dict[y_pred_bin[0]]} ({y_pred_bin[0]}) with probability \
+          {round(avg_proba[0][y_pred_bin[0]],5)} +/- {round(std_proba[0][y_pred_bin[0]],5)}")
 
     return df

src/train.py
@@ -48,6 +48,7 @@ def train(c=10, balanced=0, dual=1, ensemble_size=1, use_pairwise=True, use_scal
     df['y'] = le.fit_transform(df['chains'])\
 
     results = np.zeros((ensemble_size, 5, len(df), 3))
+    scaler_cache = {}
     model = {}
     probabilities = []
     for j in range(0, ensemble_size):
@@ -63,6 +64,7 @@ def train(c=10, balanced=0, dual=1, ensemble_size=1, use_pairwise=True, use_scal
                 X= sc.fit_transform(X)
                 # X_te = sc.transform(X_te)
                 model[f"scaler_{j}_{i}"] = sc
+                scaler_cache[f"scaler_{j}_{i}"] = sc
             clf = LogisticRegression(C=c, max_iter=1000, solver='liblinear',
                                      dual=False if dual == 0 else True,
                                      class_weight='balanced' if balanced == 1 else None)
@@ -115,8 +117,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     results, model, df = train(args.C, args.balanced, args.dual, args.ensemble_size, args.use_pairwise, args.use_scaler)
-
-
-    os.makedirs("results", exist_ok=True)
-    joblib.dump(model, "results/model.p")
-    df.to_csv(f"results/results.csv")