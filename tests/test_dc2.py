b7137afd5f8d00002ec95a370970423b2604bfec
rmadeye <rafmadaj@gmail.com>
2023-09-03 20:41:46 +0200

    sorting in pytest fixed

--- tests/test_dc2.py
+++ tests/test_dc2.py
@@ -11,13 +11,14 @@ from src.predictor import predict_oligo_state
 def test_predict_oligo_state():
     test_df = pd.DataFrame()
     test_cases = glob.glob('tests/data/*')
-    for i, test_case in enumerate(test_cases):
+    pdb_ids = [x.split('/')[-1][:4] for x in glob.glob('tests/data/*/*') if '_pair_repr_rank_001_' in x]
+    for pdb_id, test_case in zip(pdb_ids, test_cases):
         df = predict_oligo_state(test_case)
-        df['test_case_index'] = i
-        df = df.set_index('test_case_index')
-        test_df = pd.concat([test_df, df], axis=0)
+        df['pdb'] = pdb_id
+        test_df = pd.concat([test_df, df], axis=0).sort_values(by='pdb')
 
-    assertion_df = pd.read_csv('tests/test_df.csv').set_index('test_case_index')
-
-    assert np.isclose(test_df, assertion_df, atol=0.1).all(), 'Test case results do not match assertion results'
+    assertion_df = pd.read_csv('tests/test_df.csv')
+    assert np.isclose(test_df.select_dtypes(include=np.number),
+                assertion_df.select_dtypes(include=np.number),
+                atol=0.1).all(), 'Test case results do not match assertion results'
 
